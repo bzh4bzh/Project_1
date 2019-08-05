a805e1879eb507dc3a28460fe3d049ca8e44e267
itizeddieh <eddie.m.heredia@gmail.com>
2019-08-05 13:43:47 -0400

    added direct sup field

--- src/main/java/com/revature/beans/Employee.java
+++ src/main/java/com/revature/beans/Employee.java
@@ -1,12 +1,29 @@
 package com.revature.beans;
 
 public class Employee {
-	private int id;			//unique id number
-	private int reportsTo;	//unique id number of immidiate supervisor
-	private String name;	//name
+	private int id;					//unique id number
+	private int reportsTo;			//unique id number of immidiate supervisor
+	private int directSup;			//0 no, 1 YES
+	private int departmentHead;		//0 no, 1 YES
+	private int BenCon;				//0 not, 1 YES
+	private String name;			//name
 	private String username;
 	private String password;
 	
+	
+	
+	public Employee(int id, int reportsTo, int departmentHead, int benCon, String name, String username,
+			String password) {
+		super();
+		this.id = id;
+		this.reportsTo = reportsTo;
+		this.departmentHead = departmentHead;
+		BenCon = benCon;
+		this.name = name;
+		this.username = username;
+		this.password = password;
+	}
+
 	public Employee(int id, int reportsTo, String name, String username, String password) {
 		super();
 		this.id = id;
