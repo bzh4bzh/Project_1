f1cab6537d01bcad044ed5ea5f3748e9c4109caf
itizeddieh <eddie.m.heredia@gmail.com>
2019-08-06 19:27:24 -0400

    updated daos

--- src/main/java/com/revature/daoimpl/EmployeeDaoImpl.java
+++ src/main/java/com/revature/daoimpl/EmployeeDaoImpl.java
@@ -4,6 +4,7 @@ import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import com.revature.beans.Employee;
 import com.revature.dao.EmployeeDao;
 import com.revature.util.ConnFactory;
 
@@ -18,6 +19,36 @@ public class EmployeeDaoImpl implements EmployeeDao{
 	@Override
 	//returns the highest authority of a employee id
 	public String checkAthority(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getRemainingBalance(int id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getDepartmentID(int id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Employee getUser(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setRemainingBalance(int id, double bal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String authenticate(String username) {
 		// TODO Auto-generated method stub
 		return null;
 	}
