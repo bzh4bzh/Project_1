a2de7689e06959659dacb8a84137aa11c9d3a6be
John Michel <michel.john013@gmail.com>
2019-08-07 10:00:41 -0400

    requestdaoimp

--- src/main/java/com/revature/dao/RequestsDao.java
+++ src/main/java/com/revature/dao/RequestsDao.java
@@ -3,10 +3,10 @@ package com.revature.dao;
 public interface RequestsDao {
 	
 	public abstract void insertRequest(int userID); 
-	public abstract String getApplicationStatus();
+	public abstract String getApplicationStatus(int id);
 	public abstract String getPendingSuper();
 	public abstract String getPendingDeptHead();
 	public abstract String getPendingBenCo();
-	public abstract String updateStatus();
-	public abstract String updateReimbursement();
+	public abstract void updateStatus(int authority, int recid);
+	public abstract void updateReimbursement(int id, double bal);
 }
--- src/main/java/com/revature/daoimpl/RequestsDaoImipl.java
+++ src/main/java/com/revature/daoimpl/RequestsDaoImipl.java
@@ -2,21 +2,36 @@ package com.revature.daoimpl;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.revature.dao.RequestsDao;
+import com.revature.util.ConnFactory;
 
 public class RequestsDaoImipl implements RequestsDao{
-
+	public static ConnFactory cf= ConnFactory.getInstance();
 	@Override
 	public void insertRequest(int userID) {
 		
 	}
 
 	@Override
-	public String getApplicationStatus() {
-		// TODO Auto-generated method stub
-		return null;
+	public String getApplicationStatus(int id) {
+			Connection conn = cf.getConnection();
+			String sql = "select status from request inner join employee on request.userid=employee.userid where employee.userid=?";
+			PreparedStatement ps;
+			try {
+				ps = conn.prepareStatement(sql);
+				ps.setInt(1, id);
+				ResultSet rs = ps.executeQuery();
+				rs.next();
+				return rs.getString(1);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
 	}
 
 	@Override
@@ -38,15 +53,39 @@ public class RequestsDaoImipl implements RequestsDao{
 	}
 
 	@Override
-	public String updateStatus() {
-		// TODO Auto-generated method stub
-		return null;
+	public void updateStatus(int authority, int recid) {
+		Connection conn = cf.getConnection();
+		String sql = "{ call updateStatus(?,?)";
+		CallableStatement call;
+		try {
+			call = conn.prepareCall(sql);
+			call.setInt(1, authority);
+			call.setDouble(2, recid);
+			call.executeQuery();
+			
+			
+		} catch (SQLException e) {
+			//
+			e.printStackTrace();
+		}
+		
 	}
-
 	@Override
-	public String updateReimbursement() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public void updateReimbursement(int id, double bal) {
+		
+			Connection conn = cf.getConnection();
+			String sql = "{ call updateReimburse(?,?)";
+			CallableStatement call;
+			try {
+				call = conn.prepareCall(sql);
+				call.setInt(1, id);
+				call.setDouble(2, bal);
+				call.executeQuery();
+				
+			} catch (SQLException e) {
+				//
+				e.printStackTrace();
+			}
 
-}
+		}
+	}
