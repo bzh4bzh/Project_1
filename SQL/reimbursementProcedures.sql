60e837334099374debb44b1939a7c41ddfa43165
cedieme <catherine.e.diemer@gmail.com>
2019-08-06 20:50:50 -0400

    added stored procedures to change reimbursement balance and status

--- SQL/reimbursementProcedures.sql
+++ SQL/reimbursementProcedures.sql
@@ -25,9 +25,6 @@ END;
 /
 */
 
-
-
-
 /*authenticates password*/
 --select pass from employee where username=uname;
 
@@ -35,10 +32,14 @@ END;
 --Select userid from employee where username=uname;
 
 /*gets title*/
---Select titlename from title inner join employee on title.titleid=employee.authority where employee.userid=8;
+--Select titlename from title inner join employee on title.titleid=employee.authority where employee.userid=useid;
 
 /*gets department name*/
---Select deptname from title inner join employee on title.titleid=employee.authority where employee.userid=useid;
+--Select deptname from title inner join employee on department.deptid=employee.department where employee.userid=useid;
+
+/*get application status*/
+--select status from request inner join employee on request.userid=employee.userid where ?=?;
+
 
 CREATE OR REPLACE PROCEDURE insertRec(useId number, ename varchar2, eloc varchar2,
     edate date, edesc varchar2, etype number, gscale number, pgrade varchar2, justifi varchar2,
@@ -56,4 +57,15 @@ update employee set remainingBal=bal where userid=useid;
 commit;
 END;
 /
-
+CREATE OR REPLACE PROCEDURE updateReimburse(recid in number, bal in number)
+AS BEGIN
+update request set reimbursement=bal where requestid=recid;
+commit;
+END;
+/
+CREATE OR REPLACE PROCEDURE updateStatus(authority in number, recid in number)
+AS BEGIN
+update request set status=authority where requestid=recid;
+commit;
+END;
+/
